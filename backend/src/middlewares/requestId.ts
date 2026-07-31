26fee6def67d9283bbab38cd292340b73c46ce9f
amir <amir.web.codes@gmail.com>
2026-07-31 10:39:50 +0330

    feat(middleware): add requestId middleware

---
+++ backend/src/middlewares/requestId.ts
@@ -0,0 +1,8 @@
+import { randomUUID } from "crypto"
+import type { Request, Response, NextFunction } from "express"
+
+export default function (req: Request, res: Response, next: NextFunction) {
+
+    req.requestId = randomUUID()
+    next()
+}
