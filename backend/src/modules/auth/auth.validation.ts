dea0cdc87dcda2e39f3d166521e58dc19aaf15e0
amir <amir.web.codes@gmail.com>
2026-08-04 12:54:40 +0330

    feat(validation): add auth validation for sendOtp

--- backend/src/modules/auth/auth.validation.ts
+++ backend/src/modules/auth/auth.validation.ts
@@ -1,12 +1,38 @@
-// import { z } from "zod"
+import { z } from "zod";
 
-// export const sendOtpSchema = z.object({
-//     phone: z
-//         .string()
-//         .trim()
-//         .transform(normalizePhone)
-//         .refine(
-//             (phone) => /^09\d{9}$/.test(phone),
-//             "شماره موبایل معتبر نیست"
-//         ),
-// });
+function normalizePhone(phone: string) {
+    let value = phone.trim();
+
+    value = value.replace(/[\s\-()]/g, "");
+
+    if (value.startsWith("0098")) {
+        value = "+98" + value.slice(4);
+    }
+
+    if (value.startsWith("98")) {
+        value = "+" + value;
+    }
+
+    if (value.startsWith("09")) {
+        value = "+98" + value.slice(1);
+    }
+
+    return value;
+}
+
+
+const sendOtpSchema = z.object({
+    phone: z
+        .string()
+        .transform(normalizePhone)
+        .refine(
+            (phone) => /^\+989\d{9}$/.test(phone),
+            {
+                message: "Invalid phone number"
+            }
+        )
+});
+
+export {
+    sendOtpSchema
+}
