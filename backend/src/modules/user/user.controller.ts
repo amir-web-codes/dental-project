3217f72997e16935c57dd71c815739d604634f32
amir <amir.web.codes@gmail.com>
2026-08-14 16:33:50 +0330

    feat(user): update user.controller file for request endpoints

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express";
+import type { RequestListQueryDto } from "./user.dto";
 import * as userService from "./user.service";
 import checkRequest from "../../utils/checkRequest";
 import validateId from "../../utils/validateId";
@@ -50,9 +51,49 @@ async function deleteUserById(req: Request, res: Response) {
     })
 }
 
+async function createRequest(req: Request, res: Response) {
+    const user = checkRequest(req)
+
+    const data = await userService.createRequest(user.id, req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "request submitted successfully",
+        data
+    });
+}
+
+async function listRequests(req: Request, res: Response) {
+    const query = req.query as unknown as RequestListQueryDto;
+    const { data, meta } = await userService.listRequests(query);
+
+    res.json({
+        success: true,
+        message: "requests fetched successfully",
+        data,
+        meta
+    });
+}
+
+async function reviewRequest(req: Request, res: Response) {
+    const user = checkRequest(req)
+    const id = validateId(req)
+
+    const data = await userService.reviewRequest(id, user.id, req.body);
+
+    res.json({
+        success: true,
+        message: "request reviewed successfully",
+        data
+    });
+}
+
 export {
     getUserProfile,
     updateUserProfile,
     getUserDetailForAdmin,
-    deleteUserById
+    deleteUserById,
+    createRequest,
+    listRequests,
+    reviewRequest
 };
