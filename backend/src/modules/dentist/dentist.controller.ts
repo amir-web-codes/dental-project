895e5debbc67c8649aa131e990f8590916b2efe8
amir <amir.web.codes@gmail.com>
2026-08-14 15:40:48 +0330

    feat(dentist): add dentist controller file

---
+++ backend/src/modules/dentist/dentist.controller.ts
@@ -0,0 +1,59 @@
+import type { Request, Response } from "express";
+import * as dentistService from "./dentist.service";
+import type { DentistListQueryDto } from "./dentist.dto";
+import checkRequest from "../../utils/checkRequest";
+
+async function listDentists(req: Request, res: Response) {
+    const user = checkRequest(req)
+    const isAdmin = user.role === "ADMIN";
+    const query = req.query as unknown as DentistListQueryDto;
+
+    const { data, meta } = await dentistService.listDentists(query, isAdmin);
+
+    res.json({
+        success: true,
+        message: "dentists fetched successfully",
+        data,
+        meta
+    });
+}
+
+async function getMyProfile(req: Request, res: Response) {
+    const user = checkRequest(req)
+    const data = await dentistService.getMyDentistProfile(user.id);
+
+    res.json({
+        success: true,
+        message: "dentist profile fetched successfully",
+        data
+    });
+}
+
+async function updateMyProfile(req: Request, res: Response) {
+    const user = checkRequest(req)
+    const data = await dentistService.updateMyDentistProfile(user.id, req.body);
+
+    res.json({
+        success: true,
+        message: "dentist profile updated successfully",
+        data
+    });
+}
+
+async function requestVerification(req: Request, res: Response) {
+    const user = checkRequest(req)
+    const data = await dentistService.requestVerification(user.id);
+
+    res.json({
+        success: true,
+        message: "verification request submitted successfully",
+        data
+    });
+}
+
+export {
+    listDentists,
+    getMyProfile,
+    updateMyProfile,
+    requestVerification
+};
